4b83b9f1b29c3265a89974c5f295ddb15dbcad8d
zergity <zergity@gmail.com>
2022-05-05 09:40:38 +0700

    implement tax

--- contracts/SGold.sol
+++ contracts/SGold.sol
@@ -1,11 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SGold is ERC20, Ownable {
-    constructor() ERC20("Stable Gold", "SGold") {
+contract SGold is ERC20PresetMinterPauser {
+    event NewTaxRecipient(address recipient);
+    event NewTax(uint base, uint rate);
+
+    bytes32 public constant TAXER_ROLE = keccak256("TAXER_ROLE");
+    uint public constant TAX_RATE_UNIT = 1000000;
+
+    uint taxBase;
+    uint taxRate;
+    address taxRecipient;
+
+    constructor() ERC20PresetMinterPauser("Stable Gold", "SGold") {
+        taxRecipient = _msgSender();
+        emit NewTaxRecipient(taxRecipient);
+    }
+
+    function setTaxRecipient(address recipient) external {
+        require(hasRole(TAXER_ROLE, _msgSender()), "SGold: !taxer");
+        taxRecipient = recipient;
+        emit NewTaxRecipient(recipient);
+    }
+
+    function setTax(uint base, uint rate) external {
+        require(hasRole(TAXER_ROLE, _msgSender()), "SGold: !taxer");
+        taxBase = base;
+        taxRate = rate;
+        emit NewTax(base, rate);
     }
 
     /**
@@ -18,7 +43,7 @@ contract SGold is ERC20, Ownable {
      */
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
         address owner = _msgSender();
-        _transfer(owner, to, amount);
+        _transferTaxed(owner, to, amount);
         return true;
     }
 
@@ -45,7 +70,17 @@ contract SGold is ERC20, Ownable {
     ) public virtual override returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
-        _transfer(from, to, amount);
+        _transferTaxed(from, to, amount);
         return true;
+    }
+
+    function _transferTaxed(
+        address from,
+        address to,
+        uint amount
+    ) internal {
+        uint tax = taxBase + amount * taxRate / TAX_RATE_UNIT;
+        _transfer(from, taxRecipient, tax);
+        _transfer(from, to, amount - tax);
     }
 }
